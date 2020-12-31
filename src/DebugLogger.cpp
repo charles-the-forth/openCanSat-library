d9440eddc308b4318e5aeb277f7686e34329bac5
Vaclav Pavlicek <venda.pavlicek@gmail.com>
2020-12-31 08:40:31 +0100

    Move control logic to DebugLogger.

--- src/DebugLogger.cpp
+++ src/DebugLogger.cpp
@@ -1,58 +1,77 @@
 #include <DebugLogger.h>
 
+DebugLogger::DebugLogger(bool debugLogEnabled, int pcBaudrate)
+{
+    this->debugLogEnabled = debugLogEnabled;
+    this->pcBaudrate = pcBaudrate;
+}
+
+void DebugLogger::begin()
+{
+    SerialUSB.begin(this->pcBaudrate);
+}
+
+void DebugLogger::log(String message)
+{
+    if (this->debugLogEnabled)
+    {
+        SerialUSB.println(message);
+    }
+}
+
 void DebugLogger::printAllTransferedData(int messageId, OcsStorage::message1 message1, OcsStorage::message2 message2, OcsStorage::message3 message3, OcsStorage::message4 message4)
 {
-    SerialUSB.println("Message id: " + String(messageId));
+    log("Message id: " + String(messageId));
 
-    SerialUSB.println("Light intensity: " + String(message1.lightIntensity));
+    log("Light intensity: " + String(message1.lightIntensity));
 
-    SerialUSB.println("UV sensor: " + String(message2.uvIndex));
+    log("UV sensor: " + String(message2.uvIndex));
 
-    SerialUSB.println("Temperature CanSat: " + String(message1.temperatureCanSat));
-    SerialUSB.println("Temperature MPU: " + String(message2.temperatureMPU));
-    SerialUSB.println("Temperature External: " + String(message1.temperatureExternal));
-    SerialUSB.println("Temperature SCD30: " + String(message2.temperatureSCD30));
-    SerialUSB.println("Ambient temperature: " + String(message1.ambientTemp));
-    SerialUSB.println("Object temperature: " + String(message1.objectTemp));
+    log("Temperature CanSat: " + String(message1.temperatureCanSat));
+    log("Temperature MPU: " + String(message2.temperatureMPU));
+    log("Temperature External: " + String(message1.temperatureExternal));
+    log("Temperature SCD30: " + String(message2.temperatureSCD30));
+    log("Ambient temperature: " + String(message1.ambientTemp));
+    log("Object temperature: " + String(message1.objectTemp));
 
-    SerialUSB.println("Pressure CanSat: " + String(message1.pressureCanSat));
-    SerialUSB.println("Pressure External: " + String(message1.pressureExternal));
+    log("Pressure CanSat: " + String(message1.pressureCanSat));
+    log("Pressure External: " + String(message1.pressureExternal));
 
-    SerialUSB.println("Humidity CanSat: " + String(message1.humidityCanSat));
-    SerialUSB.println("Humidity External: " + String(message1.humidityExternal));
-    SerialUSB.println("Humidity SCD30: " + String(message2.humiditySCD30));
+    log("Humidity CanSat: " + String(message1.humidityCanSat));
+    log("Humidity External: " + String(message1.humidityExternal));
+    log("Humidity SCD30: " + String(message2.humiditySCD30));
 
-    SerialUSB.println("Altitude CanSat: " + String(message1.altitudeCanSat));
-    SerialUSB.println("Altitude External: " + String(message1.altitudeExternal));
+    log("Altitude CanSat: " + String(message1.altitudeCanSat));
+    log("Altitude External: " + String(message1.altitudeExternal));
 
-    SerialUSB.println("O2: " + String(message2.o2Concentration) + " %");
-    SerialUSB.println("Acceleration X: " + String(message3.accelerationX));
-    SerialUSB.println("Acceleration Y: " + String(message3.accelerationY));
-    SerialUSB.println("Acceleration Z: " + String(message3.accelerationZ));
+    log("O2: " + String(message2.o2Concentration) + " %");
+    log("Acceleration X: " + String(message3.accelerationX));
+    log("Acceleration Y: " + String(message3.accelerationY));
+    log("Acceleration Z: " + String(message3.accelerationZ));
 
-    SerialUSB.println("Rotation X: " + String(message3.rotationX));
-    SerialUSB.println("Rotation Y: " + String(message3.rotationY));
-    SerialUSB.println("Rotation Z: " + String(message3.rotationZ));
+    log("Rotation X: " + String(message3.rotationX));
+    log("Rotation Y: " + String(message3.rotationY));
+    log("Rotation Z: " + String(message3.rotationZ));
 
-    SerialUSB.println("Magnetometer X: " + String(message3.magnetometerX));
-    SerialUSB.println("Magnetometer Y: " + String(message3.magnetometerY));
-    SerialUSB.println("Magnetometer Z: " + String(message3.magnetometerZ));
+    log("Magnetometer X: " + String(message3.magnetometerX));
+    log("Magnetometer Y: " + String(message3.magnetometerY));
+    log("Magnetometer Z: " + String(message3.magnetometerZ));
 
-    SerialUSB.println("CO2 SCD30: " + String(message1.co2SCD30) + " ppm");
-    SerialUSB.println("CO2 CCS811: " + String(message1.co2CCS811) + " ppm");
-    SerialUSB.println("TVOC CCS811: " + String(message2.tvoc) + " ppb");
+    log("CO2 SCD30: " + String(message1.co2SCD30) + " ppm");
+    log("CO2 CCS811: " + String(message1.co2CCS811) + " ppm");
+    log("TVOC CCS811: " + String(message2.tvoc) + " ppb");
 
-    SerialUSB.println("A: " + String(message4.a));
-    SerialUSB.println("B: " + String(message4.b));
-    SerialUSB.println("C: " + String(message4.c));
-    SerialUSB.println("D: " + String(message4.d));
-    SerialUSB.println("E: " + String(message4.e));
-    SerialUSB.println("F: " + String(message4.f));
-    SerialUSB.println("G: " + String(message4.g));
-    SerialUSB.println("H: " + String(message4.h));
-    SerialUSB.println("I: " + String(message4.i));
-    SerialUSB.println("J: " + String(message4.j));
-    SerialUSB.println("R: " + String(message4.r));
-    SerialUSB.println("S: " + String(message4.s));
-    SerialUSB.println("T: " + String(message4.t));
+    log("A: " + String(message4.a));
+    log("B: " + String(message4.b));
+    log("C: " + String(message4.c));
+    log("D: " + String(message4.d));
+    log("E: " + String(message4.e));
+    log("F: " + String(message4.f));
+    log("G: " + String(message4.g));
+    log("H: " + String(message4.h));
+    log("I: " + String(message4.i));
+    log("J: " + String(message4.j));
+    log("R: " + String(message4.r));
+    log("S: " + String(message4.s));
+    log("T: " + String(message4.t));
 }
--- src/DebugLogger.h
+++ src/DebugLogger.h
@@ -6,7 +6,14 @@
 class DebugLogger
 {
 public:
+    DebugLogger(bool debugLogEnabled, int pcBaudrate);
+
+    void begin();
+    void log(String message);
     void printAllTransferedData(int messageId, OcsStorage::message1 message1, OcsStorage::message2 message2, OcsStorage::message3 message3, OcsStorage::message4 message4);
+private:
+    bool debugLogEnabled = false;
+    int pcBaudrate = 9600;
 };
 
 #endif /* _DEBUGLOGGER_HH */
